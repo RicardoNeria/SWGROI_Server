51f876520a8ebfac545cf646eafaa2be22639cca
Ricardo Neria <androsk_7@hotmail.com>
2025-09-13 15:11:02 -0600

    p2

--- Codigo/RetroalimentacionController.cs
+++ Codigo/RetroalimentacionController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:68a0a554882f28eb3783762e6b4c723881f815128b4f35ec27ce3768889e7899
-size 12483
+oid sha256:f1f570a1bcc7a7aacfdce276d60782eef21fb7acafb2e44aea6f1a7d7b414753
+size 13552
--- ops/backup/db_backup.ps1
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:24714c00109057f10511acf24dfbeeb0cd59e4a8bc9faba54e608d847277686a
-size 1452
