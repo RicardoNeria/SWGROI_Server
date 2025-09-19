9164f748292834ad2eb945586081423fdf6a1b7f
Ricardo Neria <androsk_7@hotmail.com>
2025-09-19 02:23:08 -0600

    p24

--- wwwroot/Scripts/login.js
+++ wwwroot/Scripts/login.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfad348e37988638bec607a1ebe85e77c68108f10d52a3597d2eaca18bb06cee
-size 3317
+oid sha256:730bd2f7a74616b05d944548af0268996839f694fab28f9b14efbeb3551f3194
+size 7426
