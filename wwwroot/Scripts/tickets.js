60e367aa2a3c61d0685054e036dc4db8a4dd91c5
Ricardo Neria <androsk_7@hotmail.com>
2025-09-20 03:22:02 -0600

    JS: usar window.safeFetch en tickets.js para cargar/listar y eliminar; manejo consistente de errores

--- wwwroot/Scripts/tickets.js
+++ wwwroot/Scripts/tickets.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7d082db675579f12aa435fa0267739d83f0abc228e3831396ee8e44664e765a
-size 18318
+oid sha256:4f5833f15f15105f704cdd5250cbc3a565d359ceae8c3f55e036df98b6ba158f
+size 18559
