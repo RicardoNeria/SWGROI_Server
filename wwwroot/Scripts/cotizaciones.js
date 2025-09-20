0b46a4267ad4101acc0874aab3ad142a0e61e14f
Ricardo Neria <androsk_7@hotmail.com>
2025-09-20 03:19:18 -0600

    JS: usar window.safeFetch en cotizaciones; limpieza y manejo de errores

--- wwwroot/Scripts/cotizaciones.js
+++ wwwroot/Scripts/cotizaciones.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd3becd5545f04f823e5d80e2ce95437295addfecc40f0a53c919f63fc9d7d6d
-size 7981
+oid sha256:3383e5b5856d9aebece6e135833ffeef445f40e3afbd170164853396c5d7ed86
+size 6283
