8b90c0724b0081bc0d676f37c9e09879ddbfd769
Ricardo Neria <androsk_7@hotmail.com>
2025-09-20 03:31:29 -0600

    JS: recuperar.js - usar safeFetchMessage y normalizar manejo de errores/feedback

--- wwwroot/Scripts/recuperar.js
+++ wwwroot/Scripts/recuperar.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b77058e02959edda4e36fa737896d167cf56a3440bc5aa35f4bb0b67058c78a
-size 2227
+oid sha256:a920cb6a21cd7439d07dbaddbdfe3d917611d37da78e389724334bd1c39b0038
+size 2726
