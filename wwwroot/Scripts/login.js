911f4f387bf4dce4471d86be6874cc25d282800c
Ricardo Neria <androsk_7@hotmail.com>
2025-09-20 03:28:01 -0600

    JS: login.js - usar safeFetchMessage y unificar manejo de errores; asegurar unlock del botón

--- wwwroot/Scripts/login.js
+++ wwwroot/Scripts/login.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd250bac0a42d612ee560fdb9d630809ca304bc6f2d292b820c30da10fd4f3d6
-size 7903
+oid sha256:2babff295adf6062341c9b27b6c163ce757bcbf929f87f3628e592e52afe9018
+size 8283
