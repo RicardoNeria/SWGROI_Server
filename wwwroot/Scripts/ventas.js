c0af7abe82a30513d5847000bce5db35f1fdca49
Ricardo Neria <androsk_7@hotmail.com>
2025-09-23 19:50:20 -0600

    p39moduloventasp3

--- wwwroot/Scripts/ventas.js
+++ wwwroot/Scripts/ventas.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:579134ecb805bef4829b55da045187e1f4b475a9e85f49cfbd9065e95c0ce399
-size 28827
+oid sha256:8f75b462e288a0043d4f3061cef1c06f894c5429af2baa44201d4624d08cf20e
+size 35818
