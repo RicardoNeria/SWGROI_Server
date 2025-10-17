fe94c89f0cae2ad03faed49e08edfcf93cbe7c0a
Ricardo Neria <androsk_7@hotmail.com>
2025-10-17 02:24:29 -0600

    fix(tickets): mostrar tipo_asunto real desde Seguimiento y abrir 'Leer más' solo en Descripción
    - Seguimiento: SELECT incluye tipo_asunto y JSON envía TipoAsunto
    - Tabla: pinta TipoAsunto; 'js-leer-mas' sólo en Descripción
    - Listener: modal se abre sólo cuando el click es en columna Descripción

--- Controladores/SeguimientoController.cs
+++ Controladores/SeguimientoController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ee1e7c66a71e28b085c12462b4e312f0fe16faeac7a33242cd74b14a02ffd95
-size 7277
+oid sha256:9c9bb83452834d9436f74178915ee73fc8838d5a5f6b022feaf26871671d8b63
+size 7212
