bfc6784431c4e72a092659c752a821b489f0eb28
Ricardo Neria <androsk_7@hotmail.com>
2025-10-17 02:45:09 -0600

    debug(tickets): agregar logging detallado para tipo_asunto por ticket
    - Console.log individual para cada ticket mostrando TipoAsunto
    - Script SQL para limpiar e insertar tickets de prueba con tipo_asunto válido
    - Permitirá identificar exactamente qué datos llegan del servidor

--- wwwroot/Scripts/tickets.js
+++ wwwroot/Scripts/tickets.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72e2cad89d6f74a4b54af59754c11418409911ea238ce3c164c8a11f2dd4f519
-size 34879
+oid sha256:26d857cebd42959f09dd03b932bf4f32e69bbfb9d65cd55f66468151c505d0a0
+size 35055
