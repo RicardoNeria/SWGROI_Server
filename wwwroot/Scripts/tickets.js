47e37b206f802d4875c9f65c74fd5ce644ec1995
Ricardo Neria <androsk_7@hotmail.com>
2025-10-17 01:58:03 -0600

    fix(tickets): evitar apertura de modal Leer más al hacer click en botones
    - Reemplazar e.stopPropagation() por e.stopImmediatePropagation() en listener de acciones de tabla
    - Evitar que listener de 'leer más' se ejecute cuando se hace click en botones (coincide con patrón ventas)

--- wwwroot/Scripts/tickets.js
+++ wwwroot/Scripts/tickets.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b15a64d0219b5cdb9cc4299167c45c87eac4ce8796ed8942bb500e387319da4c
-size 34388
+oid sha256:6eea1eb87daaaeada860a55f111821f5881dbb6a15157b6416ab1760b4ebfecf
+size 34705
