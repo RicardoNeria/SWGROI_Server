d021c951f25172e6ad646f887f977ef06f2510f3
Ricardo Neria <androsk_7@hotmail.com>
2025-10-21 11:38:31 -0600

    0023

--- wwwroot/Scripts/admin.js
+++ wwwroot/Scripts/admin.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7408147f0f2824dea8c56488150726c4d6d73177d27dab947ca48481f607896e
-size 28827
+oid sha256:9665fe661431b0eb3eca16bb0efcfefe1632ab2e9c4c4abbe44165efa9166cc5
+size 29121
