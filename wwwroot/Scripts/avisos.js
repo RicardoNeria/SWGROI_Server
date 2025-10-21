3f5596ed5fe321baad468e184f771881a88fcfd2
Ricardo Neria <androsk_7@hotmail.com>
2025-10-21 17:22:19 -0600

    0033sistema completo v3

--- wwwroot/Scripts/avisos.js
+++ wwwroot/Scripts/avisos.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43721a4174230275df9478d32162d52d307cafd1f8adb79c687be4e921cfee92
-size 30219
+oid sha256:00b78839bb5b94a646468be4ae1e746f692e7c934be5a6be2e61b62e7584b3a2
+size 30414
